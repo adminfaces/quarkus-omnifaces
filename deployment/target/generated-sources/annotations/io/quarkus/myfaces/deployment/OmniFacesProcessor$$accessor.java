dd6a782cb67b3da31760a9f95f16b6a2e69d4dc4
rafael-pestano <rafael-pestano@procergs.rs.gov.br>
2019-06-05 09:32:50 -0300

    add git ignore

--- deployment/target/generated-sources/annotations/io/quarkus/myfaces/deployment/OmniFacesProcessor$$accessor.java
+++
@@ -1,7 +0,0 @@
-package io.quarkus.myfaces.deployment;
-public final class OmniFacesProcessor$$accessor {
-    private OmniFacesProcessor$$accessor() {}
-    public static Object construct() {
-        return new OmniFacesProcessor();
-    }
-}
